1c7df4e86d94c23f6d27be8be077a31e552aecb8
B VIDEEP KUMAR REDDY <55957530+videep1@users.noreply.github.com>
2021-03-12 11:43:40 +0530

    Delete shock.py

--- shock.py
+++
@@ -1,6 +0,0 @@
-import webbrowser,time,random
-while True:
-    sites=random.choice(['google.com','youtube.com','amazon.com'])
-    visit="http://{}".format(sites)
-    webbrowser.open(visit)
-    seconds=random.randrange()
